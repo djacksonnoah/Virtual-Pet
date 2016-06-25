e3d705a905add448349adb8656ac9fb909252317
djacksonnoah <djacksonnoah@gmail.com>
2016-06-25 00:32:59 -0400

    done for the day friday

--- Virtual Pet/Program.cs
+++ Virtual Pet/Program.cs
@@ -4,12 +4,36 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Virtual_Pet
+
+
+namespace VirtualPet
 {
     class Program
     {
+
         static void Main(string[] args)
         {
+            VirtualPet myPet = newVirtualPet();
+
+            Console.WriteLine("Sassy the Dog");
+
+            Console.WriteLine("Hunger - ");
+            Console.WriteLine("Thirst - ");
+            Console.WriteLine("Boredom - ");
+            Console.WriteLine("Tiredness - ");
+
+            while (true) ;
+            {
+                Console.WriteLine("What do you want to do?");
+                Console.WriteLine("1) Feed Sassy");
+                Console.WriteLine("2) Water Sassy");
+                Console.WriteLine("3) Play with Sassy");
+                Console.WriteLine("4) Put Sassy to sleep");
+                Console.WriteLine("5) Do nothing");
+
+                myPet.Selection = Convert.ToInt32(Console.ReadLine());
+
+            }
         }
     }
 }
---
+++ Virtual Pet/VirtualPet.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Virtual_Pet
+{
+    class VirtualPet
+    {
+        // instance variables
+        private int hunger;
+        private int thirst;
+        private int boredom;
+        private int tiredness;
+        private int selection;
+        // properties
+        public int Selection()
+        {
+           
+            return selection;
+        }
+
+
+        public int Hunger
+        {
+            get
+            {
+                return hunger;
+            }
+            set
+            {
+                hunger = value;
+            }
+        }
+
+        public int Thirst
+        {
+            get
+            {
+                return thirst;
+            }
+            set
+            {
+                if (value < 0)
+                    thirst = 1;
+                else
+                    thirst = value;
+            }
+        }
+        public int Boredom
+        {
+            get
+            {
+                return boredom;
+            }
+            set
+            {
+                boredom = value;
+            }
+        }
+
+        public int Tiredness
+        {
+            get
+            {
+                return tiredness;
+            }
+            set
+            {
+                if (value < 0)
+                    tiredness = 1;
+                else
+                    tiredness = value;
+            }
+
+        }
+
+        // methods
+        public void DisplayMessage()
+        {
+            Console.WriteLine("Hunger is ", Hunger);
+            Console.WriteLine("Thirst is ", Thirst);
+            Console.WriteLine("Boredom is ", Boredom);
+            Console.WriteLine("Tiredness is ", Tiredness);
+        }
+        
+
+    // contructors
+    public VirtualPet(int hunger, int thirst, int boredom, int tiredness)
+        {
+            Hunger = hunger;
+            Thirst = thirst;
+            Boredom = boredom;
+            Tiredness = tiredness;
+        }
+
+
+
+    }
+    
+}
